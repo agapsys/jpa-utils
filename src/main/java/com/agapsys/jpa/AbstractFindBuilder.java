b3a59ef4f2a884d3d8952f3cb8ce145cec641dd8
Leandro Oliveira <leandro@agapsys.com>
2015-11-24 00:06:22 -0300

    Fixed detected errors

--- src/main/java/com/agapsys/jpa/AbstractFindBuilder.java
+++ src/main/java/com/agapsys/jpa/AbstractFindBuilder.java
@@ -17,162 +17,17 @@
 package com.agapsys.jpa;
 
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import javax.persistence.EntityManager;
 
 public abstract class AbstractFindBuilder<T extends EntityObject> extends AbstractQueryBuilder<T> {
 	// CLASS SCOPE =============================================================
-	private static class WhereClause {
-		public final String clause;
-		public final Map<String, Object> values;
-		
-		public WhereClause(String clause, Map<String, Object> values) {
-			this.clause = clause;
-			this.values = values;
-		}
-	}
-
-	private static class FindToken {
-		// CLASS SCOPE =========================================================
-		private static final String PARAM_NAME = "f";
-		
-		public static WhereClause generateWhereClause(List<FindToken> tokens) {
-			StringBuilder sb = new StringBuilder();
-			Map<String, Object> values = new LinkedHashMap<>();
-			
-			int paramCounter = 0;
-			
-			for (FindToken token : tokens) {
-				if (token.isAnd != null)
-					sb.append(token.isAnd ? " AND " : " OR ");
-				
-				sb.append("(");
-				
-				switch(token.operator) {
-					case IS_NOT_NULL:
-					case IS_NULL:
-					case NOT:
-						sb.append(token.operator.getSqlExpression(token.field, ""));
-						break;
-						
-					default:
-						int valueCounter = 0;
-
-						for (Object value : token.values) {
-							if (valueCounter > 0)
-								sb.append(" AND ");
-
-							String[] valueKeys;
-							String paramName;
-
-							if (value instanceof Range) {
-								Range range = (Range) value;
-								
-								valueKeys = new String[2];
-								
-								paramName = PARAM_NAME + paramCounter;
-								valueKeys[0] = ":" + paramName;
-								values.put(paramName, range.getMin());
-								paramCounter++;
-								
-								paramName = PARAM_NAME + paramCounter;
-								valueKeys[1] = ":" + paramName;
-								values.put(paramName, range.getMax());
-								paramCounter++;
-							} else {
-								valueKeys = new String[1];
-								
-								paramName = PARAM_NAME + paramCounter;
-								valueKeys[0] = ":" + paramName;
-								values.put(paramName, value);
-								paramCounter++;
-							}
-							
-							sb.append(token.operator.getSqlExpression(token.field, valueKeys));
-							valueCounter++;
-						}
-						break;
-				}
-				
-				sb.append(")");
-			}
-			
-			String whereClause = sb.toString();
-			
-			if (whereClause.isEmpty())
-				whereClause = null;
-			
-			return new WhereClause(whereClause, values);
-		}
-		// =====================================================================
-		
-		public final Boolean      isAnd;
-		public final String       field;
-		public final FindOperator operator;
-		public final Object[]     values;
-				
-		public FindToken(String alias, Boolean isAnd, String field, FindOperator operator, Object[] values) {			
-			if (alias == null || alias.trim().isEmpty())
-				throw new IllegalArgumentException("Null/Empty alias");
-			
-			if (field == null || field.trim().isEmpty())
-				throw new IllegalArgumentException("Null/Empty field");
-		
-			if (operator == null)
-				throw new IllegalArgumentException("Null operator");
-			
-			switch(operator) {
-				case IS_NOT_NULL:
-				case IS_NULL:
-				case NOT:
-					if (values.length != 0)
-						throw new IllegalArgumentException(String.format("Unary operator (%s) does not require a value", operator.name()));
-					
-					break;
-					
-				case BETWEEN:
-				case NOT_BETWEEN:
-					if (values.length == 0)
-						throw new IllegalArgumentException(String.format("Operator (%s) requires a range", operator.name()));
-					
-					for (int i = 0; i < values.length; i++) {
-						Object value = values[i];
-						
-						if (value == null)
-							throw new IllegalArgumentException("Null value at index " + i);
-						
-						if (!(value instanceof Range))
-							throw new IllegalArgumentException(String.format("Given value is not a range: %s", value.getClass().getName()));
-					}
-					break;
-				
-				default:
-					if (values.length == 0)
-						throw new IllegalArgumentException(String.format("Operator (%s) requires a value", operator.name()));
-					
-					for (int i = 0; i < values.length; i++) {
-						Object value = values[i];
-						
-						if (value == null)
-							throw new IllegalArgumentException("Null value at index " + i);
-					}
-					break;
-			}
-			
-			this.isAnd = isAnd;
-			this.field = String.format("%s.%s", alias, field);
-			this.operator = operator;
-			this.values = values;
-		}		
-	}
+	private static final String DEFAULT_PARAM_PREFIX = "f";
 	// =========================================================================
 	
 	// INSTANCE SCOPE ==========================================================
-	private final List<FindToken> tokens = new LinkedList<>();
-	private WhereClause whereClause = null;
-	private boolean whereClauseGenerated = false;
+	private WhereClauseBuilder whereBuilder = null;
 	
 	public AbstractFindBuilder(Class<T> entityClass) {
 		super(entityClass);
@@ -183,22 +38,25 @@ public abstract class AbstractFindBuilder<T extends EntityObject> extends Abstra
 	}
 	
 	
-	private AbstractFindBuilder andOrBy(boolean byClause, String field, FindOperator operator, Object...values) {
-		Boolean isAnd;
+	private String _(String field) {
+		if (field == null || field.trim().isEmpty())
+			throw new IllegalArgumentException("Null/Empty field");
 		
-		if (tokens.isEmpty() && !byClause)
+		return String.format("%s.%s", getAlias(), field);
+	}
+	
+	private AbstractFindBuilder andOrBy(boolean byClause, String field, FindOperator operator, Object...values) {		
+		if (whereBuilder == null && !byClause)
 			throw new IllegalStateException("AND cannot be called yet");
+				
+		field = _(field);
 		
-		if (tokens.isEmpty())
-			isAnd = null;
-		else
-			isAnd = true;
+		if (whereBuilder == null) {
+			whereBuilder = new WhereClauseBuilder(DEFAULT_PARAM_PREFIX, field, operator, values);
+		} else {
+			whereBuilder.and(field, operator, values);
+		}
 		
-		if (operator == null)
-			throw new IllegalArgumentException("Null operator");
-		
-		FindToken token = new FindToken(getAlias(), isAnd, field, operator, values);
-		tokens.add(token);
 		return this;
 	}
 	
@@ -226,15 +84,13 @@ public abstract class AbstractFindBuilder<T extends EntityObject> extends Abstra
 	}
 	
 	protected AbstractFindBuilder or(String field, FindOperator operator, Object...values) {
-		Boolean isAnd = false;
-		if (tokens.isEmpty())
+		
+		if (whereBuilder == null)
 			throw new IllegalStateException("OR cannot be called yet");
 		
-		if (operator == null)
-			throw new IllegalArgumentException("Null operator");
+		field = _(field);
 		
-		FindToken token = new FindToken(getAlias(), isAnd, field, operator, values);
-		tokens.add(token);
+		whereBuilder.or(field, operator, values);
 		return this;
 	}
 	
@@ -253,25 +109,21 @@ public abstract class AbstractFindBuilder<T extends EntityObject> extends Abstra
 	protected AbstractFindBuilder orderBy(String ordering) {
 		return (AbstractFindBuilder) super.orderBy(ordering);
 	}
-
-	
-	private void generateWhereClause() {
-		if (!whereClauseGenerated) {
-			whereClause = FindToken.generateWhereClause(tokens);
-			whereClauseGenerated = true;
-		}
-	}
 	
 	@Override
 	protected String getWhereClause() {
-		generateWhereClause();
-		return whereClause.clause;
+		if (whereBuilder == null)
+			return null;
+		
+		return whereBuilder.build();
 	}
 
 	@Override
 	protected Map<String, Object> getValues() {
-		generateWhereClause();
-		return whereClause.values;
+		if (whereBuilder == null)
+			return new LinkedHashMap<>();
+		
+		return whereBuilder.getValues();
 	}
 
 	protected Object find(EntityManager entityManager) {
